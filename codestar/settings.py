1461db8c8f5e066a46f833a97965f7192110a476
Dajan Plackovic <plackovicdajan@gmail.com>
2024-08-07 16:26:14 +0100

    remove debug, add host, fix Procfile

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)f^-%eok!3#89dsv9)oq!j^#3j)g9sjc0ijb%hz^z&c(%^!rh+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
