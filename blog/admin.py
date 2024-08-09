5bb65452858da59704be9217de3ea2845e5489d2
Dajan Plackovic <plackovicdajan@gmail.com>
2024-08-09 11:18:30 +0100

    friendlier admin console and other stuff

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'text']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('text',)
+
 
 # Register your models here.
-admin.site.register(Post)
 admin.site.register(Comment)
